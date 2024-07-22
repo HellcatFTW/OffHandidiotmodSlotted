f18d853363fdaffcd94a0e059bd666ca6fac34a1
BuildTools <unconfigured@null.spigotmc.org>
2024-07-22 17:11:57 +0300

    updated desc

--- obj/Debug/net8.0/OffHandidiotmod.AssemblyInfo.cs
+++ obj/Debug/net8.0/OffHandidiotmod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OffHandidiotmod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fecba8a0b881228a1409ecb096dc1a0c2b489dec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5afab849000e9a5e1ecac5ed4db8db96fdaa1df7")]
 [assembly: System.Reflection.AssemblyProductAttribute("OffHandidiotmod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OffHandidiotmod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
