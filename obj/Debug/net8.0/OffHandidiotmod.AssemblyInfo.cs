c13579fa5597e4bab1be76d6b134c70da8db75a9
HellcatFTW <devcatftw@gmail.com>
2024-07-28 18:54:33 +0300

    ingame desc update

--- obj/Debug/net8.0/OffHandidiotmod.AssemblyInfo.cs
+++ obj/Debug/net8.0/OffHandidiotmod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OffHandidiotmod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204fe5001e6eacbbbbf2e6381583b475ab268537")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddb76136cf1683dafce7d6d7f20da8918eab0235")]
 [assembly: System.Reflection.AssemblyProductAttribute("OffHandidiotmod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OffHandidiotmod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
